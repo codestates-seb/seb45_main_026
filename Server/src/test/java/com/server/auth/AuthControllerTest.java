e370f306d9377cf2b15333e3179c7e0043ccfe70
da9dac <da9dac@gmail.com>
2023-08-30 00:40:37 +0900

    :memo: docs: 인증 API 테스트 및 문서화 #58

--- Server/src/test/java/com/server/auth/AuthControllerTest.java
+++ Server/src/test/java/com/server/auth/AuthControllerTest.java
@@ -20,6 +20,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -27,18 +28,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.snippet.Attributes;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.server.auth.controller.dto.AuthApiRequest;
 import com.server.auth.jwt.service.JwtProvider;
 import com.server.auth.oauth.service.OAuthProvider;
 import com.server.auth.oauth.service.OAuthService;
 import com.server.auth.service.AuthService;
+import com.server.auth.service.dto.AuthServiceRequest;
 import com.server.domain.member.entity.Authority;
 import com.server.domain.member.entity.Member;
 import com.server.domain.member.repository.MemberRepository;
@@ -75,16 +79,16 @@ public class AuthControllerTest {
 	@Autowired
 	private MemberRepository memberRepository;
 
-	@BeforeAll
-	void addMember() {
-		Member member = Member.createMember(
-			"test@email.com",
-			passwordEncoder.encode("qwer1234!"),
-			"testname"
-		);
-
-		memberRepository.save(member);
-	}
+	// @BeforeAll
+	// void addMember() {
+	// 	Member member = Member.createMember(
+	// 		"test@email.com",
+	// 		passwordEncoder.encode("qwer1234!"),
+	// 		"testname"
+	// 	);
+	//
+	// 	memberRepository.save(member);
+	// }
 
 	@Test
 	@DisplayName("로컬 로그인 성공 테스트")
@@ -126,7 +130,7 @@ public class AuthControllerTest {
 	}
 
 	@Test
-	@DisplayName("OAuth2 구글 로그인 성공 테스트")
+	@DisplayName("OAuth2 로그인 성공 테스트")
 	void googleLogin() throws Exception {
 		//given
 		String provider = "GOOGLE";
@@ -170,9 +174,187 @@ public class AuthControllerTest {
 	}
 
 	@Test
-	@DisplayName("이메일 전송")
-	void sendEmail() {
+	@DisplayName("이메일 전송 성공 테스트")
+	void sendEmail() throws Exception {
+		//given
+		AuthApiRequest.Send send = new AuthApiRequest.Send("abcd@email.com");
+		String content = objectMapper.writeValueAsString(send);
 
+		//when
+		ResultActions signup = mockMvc.perform(
+			post("/auth/signup/email")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(content)
+		);
+
+		//then
+		signup
+			.andDo(print())
+			.andExpect(status().isNoContent());
+
+		signup
+			.andDo(
+				document(
+					"/auth/signup/email",
+					preprocessRequest(prettyPrint()),
+					preprocessResponse(prettyPrint()),
+					requestFields(
+						fieldWithPath("email").description("이메일")
+					)
+				)
+			);
+
+		ResultActions password = mockMvc.perform(
+			post("/auth/password/email")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(content)
+		);
+
+		password
+			.andDo(print())
+			.andExpect(status().isNoContent());
+
+		password
+			.andDo(
+				document(
+					"/auth/password/email",
+					preprocessRequest(prettyPrint()),
+					preprocessResponse(prettyPrint()),
+					requestFields(
+						fieldWithPath("email").description("이메일")
+					)
+				)
+			);
+	}
+
+	@Test
+	@DisplayName("이메일 인증 성공 테스트")
+	void confirmEmail() throws Exception {
+		//given
+		AuthApiRequest.Confirm confirm = new AuthApiRequest.Confirm(
+			"confirm@email.com",
+			"code1234"
+		);
+
+		String content = objectMapper.writeValueAsString(confirm);
+
+		//wheb
+		ResultActions signup = mockMvc.perform(
+			post("/auth/signup/confirm")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(content)
+		);
+
+		signup
+			.andDo(print())
+			.andExpect(status().isNoContent())
+			.andDo(
+				document(
+				"/auth/signup/confirm",
+					preprocessRequest(prettyPrint()),
+					preprocessResponse(prettyPrint()),
+					requestFields(
+						fieldWithPath("email").description("이메일"),
+						fieldWithPath("code").description("이메일로 받은 인증번호")
+					)
+				)
+			);
+
+		ResultActions password = mockMvc.perform(
+			post("/auth/password/confirm")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(content)
+		);
+
+		password
+			.andDo(print())
+			.andExpect(status().isNoContent())
+			.andDo(
+				document(
+					"/auth/password/confirm",
+					preprocessRequest(prettyPrint()),
+					preprocessResponse(prettyPrint()),
+					requestFields(
+						fieldWithPath("email").description("이메일"),
+						fieldWithPath("code").description("이메일로 받은 인증번호")
+					)
+				)
+			);
+	}
+
+	@Test
+	@DisplayName("회원가입 성공")
+	void signup() throws Exception {
+		//given
+		AuthApiRequest.SignUp signUp = new AuthApiRequest.SignUp(
+			"coding@joa.com",
+			"asdf1234!",
+			"당근"
+		);
+
+		String content = objectMapper.writeValueAsString(signUp);
+
+		//when
+		ResultActions actions = mockMvc.perform(
+			post("/auth/signup")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(content)
+		);
+
+		actions
+			.andDo(print())
+			.andExpect(status().isCreated());
+
+		actions
+			.andDo(
+				document(
+					"/auth/signup",
+					preprocessRequest(prettyPrint()),
+					preprocessResponse(prettyPrint()),
+					requestFields(
+						fieldWithPath("email").description("이메일"),
+						fieldWithPath("password").description("패스워드"),
+						fieldWithPath("nickname").description("닉네임(기본 채널명)")
+					)
+				)
+			);
+	}
+
+	@Test
+	@DisplayName("패스워드 초기화")
+	void updatePassword() throws Exception {
+		//given
+		AuthApiRequest.Reset reset = new AuthApiRequest.Reset(
+			"coding@joa.com",
+			"asdf1234!"
+		);
+
+		String content = objectMapper.writeValueAsString(reset);
+
+		//when
+		ResultActions actions = mockMvc.perform(
+			patch("/auth/password")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(content)
+		);
+
+		//then
+		actions
+			.andDo(print())
+			.andExpect(status().isNoContent());
+
+		actions
+			.andDo(
+				document(
+					"/auth/password",
+					preprocessRequest(prettyPrint()),
+					preprocessResponse(prettyPrint()),
+					requestFields(
+						fieldWithPath("email").description("이메일"),
+						fieldWithPath("password").description("변경할 패스워드")
+					)
+				)
+			);
 	}
 
 	private Member createMember(String email, String password) {
