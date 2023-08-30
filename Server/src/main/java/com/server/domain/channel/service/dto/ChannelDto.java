31de4bf4fdc8dc9b008e023bee97df60857956a7
kimhobeen <sksjsksh32@naver.com>
2023-08-30 16:24:22 +0900

    :memo: announcement restdocs 문서화 완료 #71

--- Server/src/main/java/com/server/domain/channel/service/dto/ChannelDto.java
+++ Server/src/main/java/com/server/domain/channel/service/dto/ChannelDto.java
@@ -1,5 +1,6 @@
 package com.server.domain.channel.service.dto;
 
+import com.server.domain.announcement.service.dto.request.AnnouncementCreateServiceRequest;
 import com.server.domain.category.entity.Category;
 import lombok.*;
 import org.springframework.stereotype.Component;
@@ -63,6 +64,21 @@ public class ChannelDto {
         private int price;
         private List<Category> categories;
         private LocalDate createdDate;
+    }
+
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class CreateAnnouncementApiRequest {
+        private String content;
+
+        public AnnouncementCreateServiceRequest toServiceRequest(Long memberId) {
+            return AnnouncementCreateServiceRequest.builder()
+                .memberId(memberId)
+                .content(content)
+                .build();
+        }
     }
 }
 
