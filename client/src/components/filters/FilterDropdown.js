1975c80683fea9c573ab1984258f63a45909e91a
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2023-09-14 15:35:15 +0900

    카테고리 드롭다운 디자인 수정

--- client/src/components/filters/FilterDropdown.js
+++ client/src/components/filters/FilterDropdown.js
@@ -1,18 +1,20 @@
 import React from "react";
 import styled from "styled-components";
 import tokens from "../../styles/tokens.json";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setSort, setIsPurchased, setCategory, setIsFree, setIsSubscribed } from "../../redux/createSlice/FilterSlice";
 
 const globalTokens = tokens.global;
 
 const DropdownContainer = styled.ul`
+    margin-top: ${globalTokens.Spacing4.value}px;
     width: 100px;
     display: flex;
     flex-direction: column;
     border-radius: ${globalTokens.RegularRadius.value}px;
-    gap: 1px;
     z-index: 1;
+    background-color: ${props=>props.isDark?globalTokens.Black.value:globalTokens.White.value};
+    border: 1px solid ${props=>props.isDark?globalTokens.Gray.value:globalTokens.LightGray.value};
 `
 const DropdownItem = styled.li`
     width: 100px;
@@ -20,15 +22,17 @@ const DropdownItem = styled.li`
     display: flex;
     justify-content: center;
     align-items: center;
-    background-color: white;
     border-radius: ${globalTokens.RegularRadius.value}px;
-    border: 1px solid lightgray;
+    color: ${props=>props.isDark?globalTokens.White.value : globalTokens.Black.value }; 
+    transition: 300ms;
+    cursor: pointer;
     &:hover{
-        background-color: lightgray;
+        color: ${globalTokens.Gray.value};
     }
 `
 
 export default function FilterDropdown({ options, actionName }) {
+    const isDark = useSelector(state=>state.uiSetting.isDark);
     const dispatch = useDispatch()
     const actionMap = {
         setSort,
@@ -38,9 +42,10 @@ export default function FilterDropdown({ options, actionName }) {
         setIsSubscribed
     };
     return (
-        <DropdownContainer>
+        <DropdownContainer isDark={isDark}>
             {options.map((el,idx)=>
                 <DropdownItem 
+                    isDark={isDark}
                     key={idx} 
                     onClick={()=>dispatch(actionMap[actionName](el))}>
                         {el.text}
