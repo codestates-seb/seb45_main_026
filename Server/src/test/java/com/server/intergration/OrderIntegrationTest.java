9f5becc4bb329d91d0e4674ae0ded4419f01c31a
da9dac <da9dac@gmail.com>
2023-09-18 00:34:13 +0900

    :white_check_mark: test: 주문 및 결제 API 통합 테스트 #347

---
+++ Server/src/test/java/com/server/intergration/OrderIntegrationTest.java
@@ -0,0 +1,7 @@
+package com.server.intergration;
+
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class OrderIntegrationTest extends IntegrationTest {
+}
