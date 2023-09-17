814de9402e6bd1194f8c155fa8c77148ca5b2b2a
kimhobeen <sksjsksh32@naver.com>
2023-09-17 15:12:51 +0900

    ::white_check_mark: test : question test 수정 #17

--- Server/src/test/java/com/server/module/s3/service/AwsModuleTest.java
+++ Server/src/test/java/com/server/module/s3/service/AwsModuleTest.java
@@ -44,7 +44,6 @@
          String imageUrl = awsService.getFileUrl(fileName, FileType.PROFILE_IMAGE);
 
          //then
-         System.out.println("imageUrl = " + imageUrl);
          ResponseEntity<byte[]> response = getResponseEntity(imageUrl);
          assertThat(response.getStatusCodeValue()).isEqualTo(200);
      }
@@ -105,7 +104,6 @@
          String thumbnailUrl = awsService.getFileUrl(fileName, FileType.THUMBNAIL);
 
          //then
-         System.out.println("thumbnailUrl = " + thumbnailUrl);
          ResponseEntity<byte[]> response = getResponseEntity(thumbnailUrl);
          assertThat(response.getStatusCodeValue()).isEqualTo(200);
      }
@@ -162,7 +160,6 @@
          String videoUrl = awsService.getFileUrl(fileName, FileType.VIDEO);
 
          //then
-         System.out.println("videoUrl = " + videoUrl);
          ResponseEntity<byte[]> response = getResponseEntity(videoUrl);
          assertThat(response.getStatusCodeValue()).isEqualTo(200);
      }
