db435d4866dd159d241d2b46230f28bf4c13272d
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2023-10-13 00:13:35 +0900

    클라이언트 채팅 UI

--- client/src/components/helpCenter/HelpCenter.js
+++ client/src/components/helpCenter/HelpCenter.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import tokens from '../../styles/tokens.json';
 import { IconButtonContainer, IconButtonImg } from '../../atoms/buttons/IconButtons';
 import closeWihte from '../../assets/images/icons/close/closeWhite.svg'
 import closeBlack from '../../assets/images/icons/close/closeBlack.svg';
 import { useSelector } from 'react-redux';
-import { frameInBottomToTopAnimation, frameInTopToBottomAnimation } from '../mainPageItems/frameAnimation';
+import { frameInBottomToTopAnimation } from '../mainPageItems/frameAnimation';
+import { Heading5Typo } from '../../atoms/typographys/Typographys';
+import HelpCenterChat from './HelpCenterChat';
 
 const globalTokens = tokens.global;
 
@@ -15,16 +17,22 @@ const HelpCenter = ({
 }) => {
     const isDark = useSelector(state=>state.uiSetting.isDark);
 
+    useEffect(()=>{
+
+    },[]);
+
     return (
         <HelpCenterContainer 
             className={isHelpClick?'frame-in':'frame-out'}
             isDark={isDark} 
             isHelpClick={isHelpClick}>
             <HelpCenterCloseContainer isHelpClick={isHelpClick}>
+            <HelpCenterTitle isDark={isDark}>고객센터</HelpCenterTitle>
                 <IconButtonContainer isHelpClick={isHelpClick} onClick={()=>{ setIsHelpClick(false) }}>
                     <IconButtonImg isHelpClick={isHelpClick} src={isDark?closeWihte:closeBlack}/>
                 </IconButtonContainer>
             </HelpCenterCloseContainer>
+            <HelpCenterChat/>
         </HelpCenterContainer>
     );
 };
@@ -46,6 +54,7 @@ export const HelpCenterContainer = styled.section`
     flex-direction: column;
     justify-content: start;
     align-items: center;
+    padding: ${globalTokens.Spacing8.value}px;
     &.frame-in {
         animation: ${frameInBottomToTopAnimation} 0.5s;
     }
@@ -61,6 +70,10 @@ export const HelpCenterCloseContainer = styled.div`
     align-items: center;
     opacity: ${props=>props.isHelpClick? '1' : '0' };
     visibility: ${props=>props.isHelpClick? 'visible' : 'hidden'};
+`
+export const HelpCenterTitle = styled(Heading5Typo)`
+    flex-grow: 1;
+    margin-left: ${globalTokens.Spacing16.value}px;
 `
 
 export default HelpCenter;
